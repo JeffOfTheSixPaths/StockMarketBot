a6fbe4dd013e19953f3ed373f6357c34168bbd2c
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2022-08-04 12:44:09 +0530

    Update sentimentAnalysis.py

--- ML Prediction/Sentiment/sentimentAnalysis.py
+++ ML Prediction/Sentiment/sentimentAnalysis.py
@@ -6,25 +6,26 @@ import pandas as pd
 import numpy as np
 import os
 
-train_dataset = 'need to import this later'
 
-encoder = tf.keras.layers.TextVectorization()
-encoder.adapt(train_dataset.map(lambda text, label: text))
 
-model = tf.keras.Sequential([ 
-    encoder,
-    tf.keras.layers.Embedding(
-        input_dim=len(encoder.get_vocabulary()),
-        output_dim=64,
-        mask_zero=True),
-  
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512)),
-    tf.keras.layers.Dense(64, activation = 'relu'),
-    tf.keras.layers.Dense(1)
-])
+def make_sentiment_model(train_dataset):
+    encoder = tf.keras.layers.TextVectorization()
+    encoder.adapt(train_dataset.map(lambda text, label: text))
 
-model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-              optimizer=tf.keras.optimizers.Adam(1e-4),
-              metrics=['accuracy'])
+    model = tf.keras.Sequential([ 
+        encoder,
+        tf.keras.layers.Embedding(
+            input_dim=len(encoder.get_vocabulary()),
+            output_dim=64,
+            mask_zero=True),
 
-#as of writing, this has 96.875% accuracy for extremely polar comments
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512)),
+        tf.keras.layers.Dense(64, activation = 'relu'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  optimizer=tf.keras.optimizers.Adam(1e-4),
+                  metrics=['accuracy'])
+    return model
+#as of writing, this has 96.875% accuracy for extremely polar comments with our weights
