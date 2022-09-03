3840e840c5473fe0a041cd86055484f1d5651c14
Tendo <71673094+JeffOfTheSixPaths@users.noreply.github.com>
2022-09-02 20:43:17 -0500

    minor changes

---
+++ scraper/dataToCSV.py
@@ -0,0 +1,9 @@
+#import tensorflow as tf
+#from tensorflow.keras import datasets, layers, models
+#import tensorflow.keras
+#import tensorflow_datasets as tfds
+import pandas as pd
+import numpy as np
+import os
+
+#making the data from the other files into a form more usuable for AI.
