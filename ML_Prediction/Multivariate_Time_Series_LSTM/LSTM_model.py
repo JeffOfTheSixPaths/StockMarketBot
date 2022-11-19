d6650d1e209d2a66767b9ddde9e5db553ba19ee6
Jude Saloum <judesaloum@pop-os.localdomain>
2022-11-18 20:56:54 -0600

    add multivariate lstm

--- ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
+++ ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
@@ -11,14 +11,9 @@ from keras.layers import LSTM
 #Preproccess data
 def preprocess(csv_name):
     df = pd.read_csv(csv_name, usecols=[
-        'Snippet D1','Lead Paragraph D1','Prices D1',
-        'Snippet D2','Lead Paragraph D2','Prices D2',
-        'Snippet D3','Lead Paragraph D3','Prices D3',
-        'Snippet D4','Lead Paragraph D4','Prices D4',
-        'Snippet D5','Lead Paragraph D5','Prices D5',
-        'Snippet D6','Lead Paragraph D6','Prices D6',
-        'Snippet D7','Lead Paragraph D7','Prices D7'
-        ], sep='\t').to_numpy()
-    print(df.shape)
+        'Snippet D1'], sep='\t').to_numpy()
+    
+    
+    print(df)
 
 preprocess('IDIDIT.csv')
