281729d22bd23776f684e235de6d225bc6db5f39
Jude Saloum <judesaloum@pop-os.localdomain>
2022-12-22 21:54:58 -0600

    add preprocessing function

--- ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
+++ ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
@@ -9,11 +9,28 @@ from keras.layers import LSTM
 
 
 #Preproccess data
-def preprocess(csv_name):
+def get_numpy(csv_name, label):
+    #read csv with usecols parameter
+    #not sure whether pop should instead be used
     df = pd.read_csv(csv_name, usecols=[
-        'Snippet D1'], sep='\t').to_numpy()
-    
-    
-    print(df)
+        label], sep='\t')
 
-preprocess('IDIDIT.csv')
+    #this is to make an array with shape (1,16) so that concatenation can occyr
+    arr = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]],dtype=float)
+
+    for i in range(len(df)):
+        #take the object out of the cell and take as item
+        ind = str(df.loc[i].item())
+        #remove the brackets as np.fromstring does not work with brackets
+        ind = ind.replace('[','')
+        ind = ind.replace(']','')
+        ind = ind.replace(', ', '|')
+        #concatenate
+        arr = np.concatenate((arr, [np.fromstring(ind, dtype=float, sep='|')]), axis=0)
+    arr = np.delete(arr, 0, 0)
+    return arr
+
+
+
+
+print(get_numpy('IDIDIT.csv',  'Snippet D1'))
