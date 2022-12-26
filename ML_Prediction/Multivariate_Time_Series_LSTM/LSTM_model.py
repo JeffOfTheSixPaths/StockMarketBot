1e9ade4420f8bcc9341c041ed4a1c82fa4d4e70d
Jude Saloum <judesaloum@pop-os.localdomain>
2022-12-25 20:33:18 -0600

    Start preprocessing

--- ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
+++ ML_Prediction/Multivariate_Time_Series_LSTM/LSTM_model.py
@@ -6,31 +6,41 @@ from tensorflow.keras import datasets, layers, models
 import pandas as pd
 import numpy as np
 from keras.layers import LSTM
+#from sklearn.preprocessing import MinMaxScaler
 
 
 #Preproccess data
 def get_numpy(csv_name, label):
-    #read csv with usecols parameter
-    #not sure whether pop should instead be used
+    '''
+    Since arrays are stored inside the pandas dataframe cells, to_numpy() is not going to work well
+    Instead, this function iterates through the cells, takes them as string items, then converts them into a numpy array
+    '''
     df = pd.read_csv(csv_name, usecols=[
-        label], sep='\t')
+        label], sep='\t') #read csv with usecols parameter
 
-    #this is to make an array with shape (1,16) so that concatenation can occyr
-    arr = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]],dtype=float)
+    arr = np.array([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]],dtype=float) #this is to make an array with shape (1,16) so that concatenation can occur
 
     for i in range(len(df)):
-        #take the object out of the cell and take as item
-        ind = str(df.loc[i].item())
+        
+        ind = str(df.loc[i].item()) #take the object out of the cell and take as item
+        
         #remove the brackets as np.fromstring does not work with brackets
         ind = ind.replace('[','')
         ind = ind.replace(']','')
         ind = ind.replace(', ', '|')
-        #concatenate
-        arr = np.concatenate((arr, [np.fromstring(ind, dtype=float, sep='|')]), axis=0)
-    arr = np.delete(arr, 0, 0)
+
+        arr = np.concatenate((arr, [np.fromstring(ind, dtype=float, sep='|')]), axis=0) #concatenate
+    
+    arr = np.delete(arr, 0, 0) #delete first index
+
     return arr
 
+dataset = np.concatenate((get_numpy("IDIDIT.csv", "Lead Paragraph D1"), get_numpy("IDIDIT.csv", "Lead Paragraph D2")), axis=1)
+print(dataset.shape)
+print(get_numpy("IDIDIT.csv", "Lead Paragraph D1").shape)
 
-
-
-print(get_numpy('IDIDIT.csv',  'Snippet D1'))
+def create_LSTM():
+    inputs = layers.Input(shape=(356,7,8))
+    layer1 = LSTM(128, input_shape=())(inputs)
+    outputs = layers.Dense(1, activation="relu")(layer1)
+    return keras.Model(inputs=inputs, outputs=outputs)
